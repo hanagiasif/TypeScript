eee9f8882226a15ffe5652252253ff6cf7f5ec29
hanagiasif <asifahmedhanagi01@gmail.com>
2024-10-20 14:06:46 +0530

    readonly and optional feild ?

---
+++ 02Basics/myObjects.js
@@ -0,0 +1,14 @@
+"use strict";
+// const User = {
+//   name: "asif",
+//   email: "asif@gmail.com",
+//   isActive: true,
+// };
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "12345",
+    name: "asif",
+    email: "asif@gamil.com",
+    isActive: false,
+};
+myUser.email = "asifahmed@gamil.com";
--- 02Basics/myObjects.ts
+++ 02Basics/myObjects.ts
@@ -1,30 +1,61 @@
-const User = {
-  name: "asif",
-  email: "asif@gmail.com",
-  isActive: true,
-};
+// const User = {
+//   name: "asif",
+//   email: "asif@gmail.com",
+//   isActive: true,
+// };
 
 // function createUser({ name: string, isPaid: boolean }) {}
 // createUser({ name: "Asif", isPaid: true });
 
-function createCourse(): {
-  name: string;
-  price: number;
-} {
-  return { name: "reactjs", price: 399 };
-}
+// function createCourse(): {
+//   name: string;
+//   price: number;
+// } {
+//   return { name: "reactjs", price: 399 };
+// }
 
-// ***** Type Aliases *****
+// // ***** Type Aliases *****
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: true };
+// }
+
+// createUser({ name: "", email: "", isActive: true });
+
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  credcardDetails?: number;
 };
 
-function createUser(user: User): User {
-  return { name: "", email: "", isActive: true };
-}
+let myUser: User = {
+  _id: "12345",
+  name: "asif",
+  email: "asif@gamil.com",
+  isActive: false,
+};
 
-createUser({ name: "", email: "", isActive: true });
+type cardNumber = {
+  cardNumber: string;
+};
+
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  };
+
+myUser.email = "asifahmed@gamil.com";
+// myUser._id="something" (Cannot assign to '_id' because it is a read-only property.)
 
 export {};
