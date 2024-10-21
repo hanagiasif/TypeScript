adeac251b48f48c3a46e8f5b7ad7cd1151dfb62c
hanagiasif <asifahmedhanagi01@gmail.com>
2024-10-21 14:39:55 +0530

    getter and setter

--- 03SetUp/dist/index.js
+++ 03SetUp/dist/index.js
@@ -13,8 +13,23 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Bijapur";
+    }
+    deleteToken() {
+        console.log("Token deketed");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const asif = new User("asif@gamil.com", "Asif Ahmed");
-// asif.name; (Property 'name' is private and only accessible within class 'User'.)
--- 03SetUp/src/index.ts
+++ 03SetUp/src/index.ts
@@ -9,10 +9,28 @@
 //   }
 // }
 class User {
+  private _courseCount = 1;
   readonly city: string = "Bijapur";
   constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("Token deketed");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const asif = new User("asif@gamil.com", "Asif Ahmed");
-
-// asif.name; (Property 'name' is private and only accessible within class 'User'.)
