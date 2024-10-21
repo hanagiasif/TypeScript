c0487f03eefd2a297a0484d0e017355b5a54b387
hanagiasif <asifahmedhanagi01@gmail.com>
2024-10-21 14:30:28 +0530

    classes

--- 03SetUp/dist/index.js
+++ 03SetUp/dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Asif Ahmed Hanagi");
+// class User {
+//   public email: string;
+//   //   private name: string;
+//   private name: string;
+//   readonly city:string="Bijapur"
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.city = "Bijapur";
+    }
+}
+const asif = new User("asif@gamil.com", "Asif Ahmed");
+// asif.name; (Property 'name' is private and only accessible within class 'User'.)
--- 03SetUp/src/index.ts
+++ 03SetUp/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Asif Ahmed Hanagi");
+// class User {
+//   public email: string;
+//   //   private name: string;
+//   private name: string;
+//   readonly city:string="Bijapur"
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+class User {
+  readonly city: string = "Bijapur";
+  constructor(public email: string, public name: string) {}
+}
+
+const asif = new User("asif@gamil.com", "Asif Ahmed");
+
+// asif.name; (Property 'name' is private and only accessible within class 'User'.)
